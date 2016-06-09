80cb98d9ba3a3302fdc08481a9f970d5f37b0c69
William Lundy <Willemlundy@Williams-MBP.hsd1.ca.comcast.net>
2016-06-09 13:14:51 -0700

    Upload project

--- SuperHeroPedia/ViewController.m
+++ SuperHeroPedia/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property NSArray *heroes;
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,12 +20,66 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+//    NSDictionary *hero  =   [NSDictionary dictionaryWithObjectsAndKeys:@"Batman", @"name",
+//                             @32, @"age", nil];
+//    
+//    NSDictionary *heroTwo  =   [NSDictionary dictionaryWithObjectsAndKeys:@"Captain America", @"name",
+//                             @95, @"age", nil];
+//    
+//    NSDictionary *heroThree  =   @{@"name":@"Iron Man",
+//                                    @"age":@40};
+//    
+//    self.heroes = [NSArray arrayWithObjects:hero, heroTwo, heroThree, nil];
+    
+    NSURL *url = [NSURL URLWithString:@"https://s3.amazonaws.com/mmios8week/superheroes.json"];
+    NSError *error;
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        self.heroes = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    }];
+    
+    [task resume];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    NSDictionary *hero = [self.heroes objectAtIndex:indexPath.row];
+    
+    
+    cell.textLabel.text = [hero objectForKey:@"name"];
+    cell.detailTextLabel.text = hero[@"description"];
+    
+    NSURL *url = [NSURL URLWithString:hero[@"avatar_url"]];
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            cell.imageView.image = [UIImage imageWithData:data];
+            [cell layoutSubviews];
+        });
+    }];
+    
+    [task resume];
+    
+    return cell;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.heroes.count;
 }
 
 @end
